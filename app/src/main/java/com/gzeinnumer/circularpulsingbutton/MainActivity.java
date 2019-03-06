527eaac6991b8acab672cc4c0beed433fe36d4ee
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2019-03-06 16:11:00 +0700

    Initial commit

---
+++ app/src/main/java/com/gzeinnumer/circularpulsingbutton/MainActivity.java
@@ -0,0 +1,19 @@
+package com.gzeinnumer.circularpulsingbutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickMe(View view) {
+        Toast.makeText(getApplicationContext(), "Click!!", Toast.LENGTH_SHORT).show();
+    }
+}
